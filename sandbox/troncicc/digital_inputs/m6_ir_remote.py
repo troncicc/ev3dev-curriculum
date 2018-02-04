f30891f50063854b9f14a2812fc7d30013120450
troncicc <troncicc@rose-hulman.edu>
2018-02-04 16:25:21 -0500

    I need to finish my m6.

--- sandbox/troncicc/digital_inputs/m6_ir_remote.py
+++ sandbox/troncicc/digital_inputs/m6_ir_remote.py
@@ -25,8 +25,8 @@
     -- Pressing the Back button will allow your program to end.  It should stop motors, turn on both green LEDs, and
        then print and say Goodbye.  You will need to implement a new robot method called shutdown to handle this task.
 
-Authors: David Fisher and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+Authors: David Fisher and Catianne Troncin.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import ev3dev.ev3 as ev3
 import time
@@ -34,7 +34,7 @@ import time
 import robot_controller as robo
 
 # Note that todo2 is farther down in the code.  That method needs to be written before you do todo3.
-# TODO: 3. Have someone on your team run this program on the EV3 and make sure everyone understands the code.
+# DONE: 3. Have someone on your team run this program on the EV3 and make sure everyone understands the code.
 # Can you see what the robot does and explain what each line of code is doing? Talk as a group to make sure.
 
 
@@ -58,9 +58,24 @@ def main():
     robot = robo.Snatch3r()
     dc = DataContainer()
 
-    # TODO: 4. Add the necessary IR handler callbacks as per the instructions above.
+    # DONE: 4. Add the necessary IR handler callbacks as per the instructions above.
     # Remote control channel 1 is for driving the crawler tracks around (none of these functions exist yet below).
     # Remote control channel 2 is for moving the arm up and down (all of these functions already exist below).
+    rc1 = ev3.RemoteControl(channel=1)
+    rc2 = ev3.RemoteControl(channel=2)
+    rc1.on_red_up = lambda state: handle_drive_left_forward(state, robot)
+    rc1.on_red_down = lambda state: handle_drive_left_backward(state, robot)
+    rc1.on_blue_up = lambda state: handle_drive_right_forward(state, robot)
+    rc1.on_blue_down = lambda state: handle_drive_right_backward(state, robot)
+    rc2.on_red_up = lambda state: robot.arm_up()
+    rc2.on_red_down = lambda state: robot.arm_down()
+    rc2.on_blue_up = lambda state: robot.arm_calibration()
+
+    """
+    -- Pressing red up   calls robot.arm_up().
+    -- Pressing red down calls robot.arm_down().
+    -- Pressing blue up  calls robot.arm_calibration().
+    """
 
     # For our standard shutdown button.
     btn = ev3.Button()
@@ -69,11 +84,13 @@ def main():
     robot.arm_calibration()  # Start with an arm calibration in this program.
 
     while dc.running:
-        # TODO: 5. Process the RemoteControl objects.
+        # DONE: 5. Process the RemoteControl objects.
+        rc1.process()
+        rc2.process()
         btn.process()
         time.sleep(0.01)
 
-    # TODO: 2. Have everyone talk about this problem together then pick one  member to modify libs/robot_controller.py
+    # DONE: 2. Have everyone talk about this problem together then pick one  member to modify libs/robot_controller.py
     # as necessary to implement the method below as per the instructions in the opening doc string. Once the code has
     # been tested and shown to work, then have that person commit their work.  All other team members need to do a
     # VCS --> Update project...
@@ -90,6 +107,17 @@ def main():
 # TODO: 7. When your program is complete, call over a TA or instructor to sign your checkoff sheet and do a code review.
 #
 # Observations you should make, IR buttons are a fun way to control the robot.
+    """IR remote channel 1 to drive the crawler tracks around:
+        -- Pressing  red up   makes the left  LED turn green and the left_motor  move at  600.
+             Releasing turns off the LED and stops left_motor.
+        -- Pressing  red down makes the left  LED turn red   and the left_motor  move at -600.
+             Releasing turns off the LED and stops left_motor.
+        -- Pressing  blue up  makes the right LED turn green and the right_motor move at  600.
+             Releasing turns off the LED and stops right_motor.
+        -- Pressing blue down makes the right LED turn red   and the right_motor move at -600.
+             Releasing turns off the LED and stops right_motor.
+        You should be able to use one red (left) button and one blue (right) button at the same time.  For example:
+         while pressing both red up and blue down the robot should spin and LEDs will be green (left) and red (right)."""
 
 
 def handle_arm_up_button(button_state, robot):
@@ -138,6 +166,12 @@ def handle_shutdown(button_state, dc):
     """
     if button_state:
         dc.running = False
+
+
+def handle_drive_left_forward(state, robot):
+    if state:
+        robot.left_motor.STATE_RUNNING()
+
 
 # ----------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
