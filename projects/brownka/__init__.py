170ddfb62d5c232eaa6ac21619e43eaec1b10ccb
brownka <brownka@rose-hulman.edu>
2018-02-12 08:46:38 -0500

    removed extraneous sandbox folders

---
+++ projects/brownka/__init__.py
@@ -0,0 +1,18 @@
+"""
+PROJECT DETAILS AND SETUP
+
+**NOTE: Not all of these steps necessarily need to be done in order
+
+DONE 1: Create two modules, one to run on the pc and the other to run on the robot
+
+TODO 2: Setup mqtt client to link the two modules together
+
+TODO 3: Setup classes to save information for the duration of the program in both sections
+
+TODO 4:
+
+
+Still need to decide on theme
+
+
+"""
